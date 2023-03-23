8bee684daf26658c581dd8710b2c46c1696a38d8
Emil Strømsvåg <emilchri@hotmail.com>
2023-03-23 22:02:11 +0100

    Fixed vgg16_finetuner to use image features

--- finetune_vgg16.py
+++ finetune_vgg16.py
@@ -25,16 +25,16 @@ assert (
 
 
 # optimized for RTX 4090. for larger GPUs, increase some of these?
-MICRO_BATCH_SIZE = 6  # this could actually be 5 but i like powers of 2
+MICRO_BATCH_SIZE = 3  # this could actually be 5 but i like powers of 2
 BATCH_SIZE = 128
 GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
 EPOCHS = 3  # we don't always need 3 tbh
 LEARNING_RATE = 3e-4  # the Karpathy constant
-CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
+CUTOFF_LEN = 145000 #256  # 256 accounts for about 96% of the data
 LORA_R = 8
 LORA_ALPHA = 16
 LORA_DROPOUT = 0.05
-VAL_SET_SIZE = 0  # 2000
+VAL_SET_SIZE = 600  # 2000
 TARGET_MODULES = [
     "q_proj",
     "v_proj",
@@ -77,38 +77,17 @@ tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos tok
 data = load_dataset("json", data_files=DATA_PATH)
 
 
-def get_image_features(data_point):
-    if data_point['ImageID'] != GLOBAL_LAST_PROMPT["ImageID"]:
-        image_path = f"{IMAGE_PATH}/{data_point['ImageID']}.jpg"
-        features = alpaca_image_feature_extraction.get_image_features(
-            img_path=image_path,
-            model=IMAGE_MODEL,
-            np_type=np.float16).tolist()
-
-        GLOBAL_LAST_PROMPT["ImageID"] = data_point["ImageID"]
-        GLOBAL_LAST_PROMPT["image_features"] = features
-
-    return features
-
-
 def generate_prompt(data_point):
     # sorry about the formatting disaster gotta move fast
-    if data_point["input"]:
-        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+    img_path = f"{IMAGE_PATH}/{data_point['input']}.jpg"
+    vgg16_img_features = alpaca_image_feature_extraction.get_image_features(img_path=img_path, model=IMAGE_MODEL, np_type=np.float16)
+    return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
 ### Instruction:
 {data_point["instruction"]}
 
 ### Input:
-{data_point["input"]}
-
-### Response:
-{data_point["output"]}"""
-    else:
-        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
-
-### Instruction:
-{data_point["instruction"]}
+{vgg16_img_features}
 
 ### Response:
 {data_point["output"]}"""
@@ -130,55 +109,8 @@ def tokenize(prompt):
 
 
 def generate_and_tokenize_prompt(data_point):
-    # This function masks out the labels for the input,
-    # so that our loss is computed only on the response.
-    input = data_point["input"]  # get_image_features(data_point)
-    user_prompt = (
-        (
-            f"""Below is a question that describes a task, paired with an input that provides further context. Write a response that appropriately answers the request.
-
-### Question:
-{data_point["Question"]}
-
-### Input image:
-{input}
-
-### Answer:
-"""
-        )
-        if input
-        else (
-            f"""Below is an instruction that describes a task. Write a response that appropriately answers the request.
-
-### Question:
-{data_point["Question"]}
-
-### Response:
-"""
-        )
-    )
-    len_user_prompt_tokens = (
-        len(
-            tokenizer(
-                user_prompt,
-                truncation=True,
-                max_length=CUTOFF_LEN + 1,
-            )["input_ids"]
-        )
-        - 1
-    )  # no eos token
-    full_tokens = tokenizer(
-        user_prompt + data_point["Answer"],
-        truncation=True,
-        max_length=CUTOFF_LEN + 1,
-        padding="max_length",
-    )["input_ids"][:-1]
-    return {
-        "input_ids": full_tokens,
-        "labels": [-100] * len_user_prompt_tokens
-        + full_tokens[len_user_prompt_tokens:],
-        "attention_mask": [1] * (len(full_tokens)),
-    }
+    prompt = generate_prompt(data_point)
+    return tokenize(prompt)
 
 
 if VAL_SET_SIZE > 0:
@@ -205,15 +137,14 @@ trainer = transformers.Trainer(
         logging_steps=20,
         evaluation_strategy="steps" if VAL_SET_SIZE > 0 else "no",
         save_strategy="steps",
-        eval_steps=200 if VAL_SET_SIZE > 0 else None,
-        save_steps=200,
+        eval_steps=60 if VAL_SET_SIZE > 0 else None,
+        save_steps=60,
         output_dir=OUTPUT_DIR,
         save_total_limit=3,
         load_best_model_at_end=True if VAL_SET_SIZE > 0 else False,
         ddp_find_unused_parameters=False if ddp else None,
     ),
-    data_collator=transformers.DataCollatorForLanguageModeling(
-        tokenizer, mlm=False),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
 )
 model.config.use_cache = False
 
@@ -226,7 +157,7 @@ if torch.__version__ >= "2" and sys.platform != "win32":
     model = torch.compile(model)
 
 trainer.train()
-
+print("\nFinished training!")
 model.save_pretrained(OUTPUT_DIR)
-
+print(f"\nSaved model to {OUTPUT_DIR}")
 print("\n If there's a warning about missing keys above, please disregard :)")
