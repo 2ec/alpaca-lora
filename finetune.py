fd8c407bf288fc8e17dce50c684d747d52a91051
Emil Strømsvåg <emilchri@hotmail.com>
2023-03-31 12:26:32 +0200

    test finetune

--- finetune.py
+++ finetune.py
@@ -216,7 +216,7 @@ def train(
         args=transformers.TrainingArguments(
             per_device_train_batch_size=micro_batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
-            warmup_steps=100,
+            warmup_steps=1,
             num_train_epochs=num_epochs,
             learning_rate=learning_rate,
             fp16=True,
@@ -224,8 +224,9 @@ def train(
             optim="adamw_torch",
             evaluation_strategy="steps" if val_set_size > 0 else "no",
             save_strategy="steps",
-            eval_steps=200 if val_set_size > 0 else None,
-            save_steps=200,
+            eval_steps=10 if val_set_size > 0 else None, # 200
+            save_steps=200, # 200
+            do_eval=True,
             output_dir=output_dir,
             save_total_limit=3,
             load_best_model_at_end=True if val_set_size > 0 else False,
