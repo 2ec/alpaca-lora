1662d74e0a39a0eeed0f02e94d60d267db719892
Emil Strømsvåg <emilchri@hotmail.com>
2023-04-12 14:40:24 +0200

    Add Faster R-CNN to feature extraction

--- alpaca_image_feature_extraction_torch.py
+++ alpaca_image_feature_extraction_torch.py
@@ -1,5 +1,7 @@
 from torchvision.io import read_image
 from torchvision.models import VGG16_Weights, vgg16
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+import pandas as pd
 
 
 def get_image_top_n_classes(
@@ -39,27 +41,58 @@ def get_image_predictions(
     return predictions
 
 
+def get_image_predictions_faster_rcnn(img, model, weights=FasterRCNN_ResNet50_FPN_V2_Weights, from_path=True):
+    if from_path:
+        img = read_image(img)
+
+        # Step 2: Initialize the inference transforms
+    preprocess = weights.transforms()
+
+    # Step 3: Apply inference preprocessing transforms
+    batch = preprocess(img).unsqueeze(0)
+
+    # Step 4: Use the model and visualize the prediction
+    prediction = model(batch)[0]
+    return prediction
+
+def get_image_top_n_classes_faster_rcnn(
+        img,
+        model,
+        top_n_features: int = 50,
+        weights=FasterRCNN_ResNet50_FPN_V2_Weights,
+        from_path=True,
+    ) -> list:
+    prediction = get_image_predictions_faster_rcnn(img, model, weights, from_path)
+
+    labels_scores = []
+
+    for i, score in enumerate(prediction["scores"]):
+        label = weights.meta["categories"][prediction["labels"][i]]
+        xmin, ymin, xmax, ymax = prediction["boxes"][i].detach().tolist()
+        coordinates = (int(xmin), int(ymin), int(xmax), int(ymax))
+        score = round(score.item() * 100, 3)
+        labels_scores.append([label, score, coordinates])
+    sorted_labels_scores = sort_list_of_scores(labels_scores, top_n_features)
+    return sorted_labels_scores
+
+def sort_list_of_scores(list:list, top_n_features:int) -> list:
+    df = pd.DataFrame(list, columns=["label", "score", "coordinates"])
+    df.sort_values(by=["score"], ascending=False, inplace=True)
+    return df.loc[:top_n_features].values.tolist()
+
+
 if __name__ == "__main__":
     # From https://pytorch.org/vision/stable/models.html#classification
     # Step 1: Initialize model with the best available weights
-    weights = VGG16_Weights.IMAGENET1K_V1
-    model = vgg16(weights=weights)
+    weights = FasterRCNN_ResNet50_FPN_V2_Weights
+    model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.001)
+
+    #weights = VGG16_Weights.IMAGENET1K_V1
+    #model = vgg16(weights=weights)
     model.eval()
     NUM_TOP_CATEGORIES = 100
 
     img = "ImageCLEFmed-MEDVQA-GI-2023-Development-Dataset/images/cl8k2u1pm1dw7083203g1b7yv.jpg"
 
     # Step 4: Use the model and print the predicted category
-    predictions = get_image_predictions(img_path=img, model=model)
-
-    prediction = predictions.softmax(0)
-    class_id = prediction.argmax().item()
-    score = prediction[class_id].item()
-    category_name = weights.meta["categories"][class_id]
-    print(f"Top category: {category_name}: {100 * score:.1f}%")
-
-    top_n_classes = get_image_top_n_classes(
-        img_path=img, model=model, top_n_features=NUM_TOP_CATEGORIES
-    )
-
-    print(top_n_classes)
+    get_image_top_n_classes_faster_rcnn(img=img, model=model, top_n_features=50, weights=weights, from_path=True)
