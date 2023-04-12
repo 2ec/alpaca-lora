ed4cfd32358bbf63ce3780a6e4a4624bd0a2184a
Emil Strømsvåg <emilchri@hotmail.com>
2023-04-12 11:25:33 +0200

    formated

--- alpaca_image_feature_extraction_torch.py
+++ alpaca_image_feature_extraction_torch.py
@@ -1,20 +1,30 @@
 from torchvision.io import read_image
-from torchvision.models import vgg16, VGG16_Weights
+from torchvision.models import VGG16_Weights, vgg16
 
-def get_image_top_n_classes(img, model, top_n_features:int=100, weights=VGG16_Weights.IMAGENET1K_V1, from_path=True) -> list:
+
+def get_image_top_n_classes(
+    img,
+    model,
+    top_n_features: int = 100,
+    weights=VGG16_Weights.IMAGENET1K_V1,
+    from_path=True,
+) -> list:
     predicted = get_image_predictions(img, model, weights, from_path)
     predicted_softmax = predicted.softmax(0)
 
     new_list = []
     for i in range(len(predicted_softmax)):
-        score = round(predicted_softmax[i].item()*100, 3)
+        score = round(predicted_softmax[i].item() * 100, 3)
         category_name = weights.meta["categories"][i]
         new_list.append((category_name, score))
 
     new_list_sorted = sorted(new_list, key=lambda tup: tup[1], reverse=True)
     return new_list_sorted[:top_n_features]
 
-def get_image_predictions(img, model, weights=VGG16_Weights.IMAGENET1K_V1, from_path=True):
+
+def get_image_predictions(
+    img, model, weights=VGG16_Weights.IMAGENET1K_V1, from_path=True
+):
     if from_path:
         img = read_image(img)
 
@@ -48,7 +58,8 @@ if __name__ == "__main__":
     category_name = weights.meta["categories"][class_id]
     print(f"Top category: {category_name}: {100 * score:.1f}%")
 
-    top_n_classes = get_image_top_n_classes(img_path=img, model=model, top_n_features=NUM_TOP_CATEGORIES)
+    top_n_classes = get_image_top_n_classes(
+        img_path=img, model=model, top_n_features=NUM_TOP_CATEGORIES
+    )
 
     print(top_n_classes)
-
