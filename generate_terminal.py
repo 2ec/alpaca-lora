48d7c6504af70e1cb139a23bfdb82f780547740d
Emil Strømsvåg <emilchri@hotmail.com>
2023-04-14 14:45:08 +0200

    Move score tensor to CPU before converting to numpy

--- generate_terminal.py
+++ generate_terminal.py
@@ -125,7 +125,7 @@ def evaluate(
         )
     s = generation_output.sequences[0]
     output = tokenizer.decode(s)
-    scores = generation_output.scores[0].softmax(1).detach().numpy()
+    scores = generation_output.scores[0].softmax(1).detach().cpu().numpy()
     return output, scores
 
 def alpaca_predict_lime(texts):
