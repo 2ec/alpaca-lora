6c14c0c58237535f101d6ea78c0f6ef0ed27cba1
Emil Strømsvåg <emilchri@hotmail.com>
2023-04-27 12:03:54 +0200

    print out perplexity

--- generate_terminal.py
+++ generate_terminal.py
@@ -173,6 +173,60 @@ def predictor(texts):
     probas = F.softmax(outputs.logits).detach().numpy()
     return probas
 
+
+def print_token_probs(generation_output):
+    """
+    Computes the transition scores of sequences given the generation scores 
+    (and beam indices, if beam search was used). This is a convenient method 
+    to quicky obtain the scores of the selected tokens at generation time.
+    https://huggingface.co/docs/transformers/main/en/main_classes/text_generation#transformers.GenerationMixin.compute_transition_scores
+    
+
+    Input: 
+        generation_output_sequences: output.sequences from model.generate()
+    
+    Prints out | token | token string | logits | probability
+    """
+    # Detokenize
+    s = generation_output.sequences[0]
+    output = tokenizer.decode(s)
+    # Split input from response
+    output_cleaned = output.split('### Response:')[1].strip()
+    # Tokenize response only
+    inputs = tokenizer(
+        output_cleaned,
+        return_tensors="pt",
+    )
+    input_ids = inputs["input_ids"]
+
+    # Compute transition scores
+    transition_scores = model.compute_transition_scores(
+        generation_output.sequences, generation_output.scores, normalize_logits=True
+    )
+
+    token_scores = []
+    # Print put token, token string, logits and probability
+    for tok, score in zip(input_ids[0].cpu(), transition_scores[0].cpu()):
+        # | token | token string | logits | probability
+        token = tokenizer.decode(tok)
+        token_score = np.exp(score.numpy())
+        token_scores.append([token, token_score])
+        # print(f"| {tok:5d} | {token:8s} | {score.numpy():.4f} | {token_score:.2%}")
+    return token_scores
+
+def ppl(scores):
+    """
+    Calculate perplexity
+    """
+    p_W = np.prod(scores)
+    # print("p_W", p_W)
+    Pnorm_W = p_W ** (1/len(scores))
+    # print("Pnorm_W", Pnorm_W)
+    ppl = 1/Pnorm_W
+    # print(ppl)
+    return ppl
+
+
 while True:
     instruction=input("\nEnter instruction. Press enter to exit. ")
     if len(instruction) <= 0:
@@ -185,7 +239,12 @@ while True:
     print(f"\nResponse: {output_cleaned}")
     
     print(f"\nScores:\nType{type(scores)}\nShape: {scores.shape}\nScores: {scores}")
-    print(f"\nMean_value:", np.mean(scores))
+    token_scores = print_token_probs(generation_output)
+
+    tokens, scores = map(list, zip(*token_scores))
+    perplexity = ppl(scores)
+    print(f"\nPerplexity: {perplexity}")
+
 
     see_more = input("Do you want to see the whole output? y/n: ")
     if see_more == "y":
